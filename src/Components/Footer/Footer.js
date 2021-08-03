4ee23c4350e6f2ce3c37144ecb367e728389f827
ShasheeshPurohit <75235962+ShasheeshPurohit@users.noreply.github.com>
2021-08-04 01:27:19 +0530

    Footer Links Updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,18 +7,20 @@ export default function Footer() {
       {/* <p className="footer-text">Follow us on:</p> */}
       <ul className="footer-links">
         <li>
-          <i className="fab fa-facebook-square facebook"></i>
+          <a href="https://www.linkedin.com/in/shasheeshpurohit/">
+          <i className="fab fa-linkedin linkedIn"></i></a>
         </li>
         <li>
-          <i className="fab fa-instagram instagram"></i>
+          <a href="https://www.instagram.com/shasheesh15/?hl=en"><i className="fab fa-instagram instagram"></i></a>
         </li>
         <li>
-          <i className="fab fa-twitter-square twitter"></i>
-        </li>
+          <a href="https://twitter.com/shasheeshcodes"><i className="fab fa-twitter-square twitter"></i>
+          </a></li>
         <li>
-          <i className="fab fa-github github"></i>
-        </li>
+          <a href="https://github.com/ShasheeshPurohit"><i className="fab fa-github github"></i>
+          </a></li>
       </ul>
+      Developed by: Shasheesh Purohit
     </footer>
   );
 }
