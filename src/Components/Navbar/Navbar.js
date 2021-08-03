40a7db2ec5903e074a033349a72bd9509ec4088a
ShasheeshPurohit <75235962+ShasheeshPurohit@users.noreply.github.com>
2021-08-04 00:51:39 +0530

    Responsiveness Updated

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import {useCart} from "../../Context/CartContext"
@@ -6,13 +6,49 @@ import { useAuth } from "../../Context/AuthContext";
 
 export default function Navbar() {
   const {token, logoutHandler} = useAuth();
-
   const {state} = useCart();
+
+  const [sidebar, setSidebar] = useState(false);
+
 
   return (
     <div>
+      {sidebar?
+      <div className="sidebar">
+        <div className="sidebar-brand">
+          <p>Mobilewaale</p>
+          <i class="fas fa-times" onClick={()=>setSidebar(!sidebar)}></i>
+        </div>
+        <ul className="nav-items-list-sidebar">
+            <li>
+              <Link className="nav-links-sidebar" onClick={()=>setSidebar(!sidebar)} to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-links-sidebar" onClick={()=>setSidebar(!sidebar)} to="/products">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-links-sidebar" onClick={()=>setSidebar(!sidebar)} to="/wishlist">
+              <i class="fas fa-heart"></i>
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-links-sidebar" onClick={()=>setSidebar(!sidebar)} to="/cart">
+              <i class="fas fa-shopping-cart"></i><div className="cart-length">{state!=0?state.length:""}</div>
+              </Link>
+            </li>
+            
+          </ul>
+          
+      
+      </div>:""}
       <nav className="navbar">
-        <div className="nav-brand"><Link className="nav-brand-link" to="/">Mobilewaale</Link></div>
+        <div className="nav-brand">
+        <i className="fas fa-bars sidebar-button" onClick={()=> setSidebar(!sidebar)}></i>
+          <Link className="nav-brand-link" to="/">Mobilewaale</Link></div>
         <div className="nav-items">
           <ul className="nav-items-list">
             <li>
@@ -50,6 +86,23 @@ export default function Navbar() {
             </li>
             }
             
+          </ul>
+          <ul className="nav-items-list-mobile">
+            
+            {token?<li onClick={()=>logoutHandler()}>Log out</li>:
+            <li>
+              <Link className="nav-links" to="/login">
+                Login
+              </Link>
+            </li>
+            }
+            {token?"":
+            <li>
+              <Link className="nav-links" to="/signup">
+                Sign Up
+              </Link>
+            </li>
+            }
           </ul>
           
         </div>
--- src/Context/FilterContext.js
+++ src/Context/FilterContext.js
@@ -24,8 +24,13 @@ export const FilterProvider = ({ children }) => {
 
   useEffect(() => {
     (async function(){
+      try{
       const response = await axios.get(`${baseurl}/api/products`)
     setProducts(response.data.products)
+      }
+      catch(error){
+        console.log(error)
+      }
     })()
   }, [])
 
