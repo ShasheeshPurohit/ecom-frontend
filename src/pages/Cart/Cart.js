ee8d881e06638434d38b139f93e57e0ded2f6bbc
ShasheeshPurohit <75235962+ShasheeshPurohit@users.noreply.github.com>
2021-08-03 16:44:10 +0530

    Backend integrated

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,14 +1,59 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import {Link} from "react-router-dom";
 import "./Cart.css";
 import { useCart } from "../../Context/CartContext";
+import { useAuth } from "../../Context/AuthContext";
 import ProductCard from "../../Components/Cards/Product/ProductCard";
+const axios = require('axios')
+import { baseurl } from "../../utils/apiCalls";
 
 export default function Cart() {
-  const { state, dispatch } = useCart();
+  const {state, dispatch} = useCart();
+  const {token} = useAuth();
+  const [remove, setRemove] = useState(false);
+  const [add, setAdd] = useState(false);
+  const [less, setLess] = useState(false);
+
+  //   Cart Functions
+  const increaseQuantity = async (product) => {
+    setAdd(true);
+    const response = await axios.post(`${baseurl}/api/cart/${product._id}/inc`, {});
+    console.log(response)
+    if (response.status === 200) {
+      dispatch({ type: "INCREMENT", payload: product });
+      setAdd(false);
+    }
+    setAdd(false);
+  };
+
+  const decreaseQuantity = async (product) => {
+    if (product.qty > 1) {
+      setLess(true);
+      const response = await axios.post(`${baseurl}/api/cart/${product._id}/dec`, { });
+      if (response.status === 200) {
+        dispatch({ type: "DECREMENT", payload: product });
+      }
+    } else {
+      removeFromCart(product);
+    }
+    setLess(false);
+  };
+
+  const removeFromCart = async (product) => {
+    setRemove(true);
+    const response = await axios.delete(`${baseurl}/api/cart/${product._id}`);
+    if (response.status === 200) {
+      dispatch({ type: "REMOVE", payload: product });
+      setRemove(false);
+    }
+    setRemove(false);
+  };
+
   function totalPrice(){
     let total = 0;
     state.map((item)=>{
-      total+= (item.price)*item.cartQty
+      total+= (item.price)*item.qty
     })
     return total
   }
@@ -19,16 +64,15 @@ export default function Cart() {
       <div className="cart-items">
         {state.map((item) => {
           return (
-            <div key={item.id} className="cart-product-display">
+            <div key={item._id} className="cart-product-display">
               <ProductCard
                 item={item}
+                itemName={<Link to={`/products/${item._id}`} className="product-name">{item.name}</Link>}
                 buttonText={"Remove"}
-                buttonHandler={() =>
-                  dispatch({ type: "REMOVE", payload: item })
-                }
-                quantity={item.cartQty}
-                incrementButtonHandler={()=> dispatch({type:"DECREMENT", payload:item})}
-                decrementButtonHandler={()=> dispatch({type:"INCREMENT", payload:item})}
+                buttonHandler={() =>removeFromCart(item)}
+                quantity={item.qty}
+                incrementButtonHandler={()=> increaseQuantity(item)}
+                decrementButtonHandler={()=> decreaseQuantity(item)}
                 displayIcon={"none"}
               />
             </div>
